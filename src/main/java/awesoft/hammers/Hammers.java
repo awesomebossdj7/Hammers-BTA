de0cec5be367cbae7ac8c48c3100fe0e03133c9d
DJ <70352423+OwOsucksAwesomeMinecraftbossAlissha6TRM@users.noreply.github.com>
2024-07-08 08:18:41 -0400

    hammers actually owrk

--- src/main/java/awesoft/hammers/Hammers.java
+++ src/main/java/awesoft/hammers/Hammers.java
@@ -9,6 +9,7 @@ import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.ItemBuilder;
 import turniplabs.halplibe.helper.ItemHelper;
 import turniplabs.halplibe.helper.RecipeBuilder;
 import turniplabs.halplibe.util.RecipeEntrypoint;
@@ -19,30 +20,72 @@ public class Hammers implements ModInitializer, RecipeEntrypoint {
 	public static final String MOD_ID = "hammers";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+
+
 	//hammers
-	public static final Item toolHammerWooden = ItemHelper.createItem(MOD_ID, new WoodenHammer("tool.pickaxe.hammer.wooden", HammerConfig.cfg.getInt("Item IDs.WoodenHammer"), HammerToolMaterial.wood), "tool.pickaxe.hammer.wooden", "wooden_hammer.png");
-	public static final Item toolHammerStone = ItemHelper.createItem(MOD_ID, new StoneHammer("tool.pickaxe.hammer.stone", HammerConfig.cfg.getInt("Item IDs.StoneHammer"), HammerToolMaterial.stone), "tool.pickaxe.hammer.stone", "stone_hammer.png");
-	public static final Item toolHammerIron = ItemHelper.createItem(MOD_ID, new IronHammer("tool.pickaxe.hammer.iron", HammerConfig.cfg.getInt("Item IDs.IronHammer"), HammerToolMaterial.iron), "tool.pickaxe.hammer.iron", "iron_hammer.png");
-	public static final Item toolHammerGold = ItemHelper.createItem(MOD_ID, new GoldHammer("tool.pickaxe.hammer.gold", HammerConfig.cfg.getInt("Item IDs.GoldHammer"), HammerToolMaterial.gold), "tool.pickaxe.hammer.gold", "golden_hammer.png");
-	public static final Item toolHammerDiamond = ItemHelper.createItem(MOD_ID, new DiamondHammer("tool.pickaxe.hammer.diamond", HammerConfig.cfg.getInt("Item IDs.DiamondHammer"), HammerToolMaterial.diamond), "tool.pickaxe.hammer.diamond", "diamond_hammer.png");
-	public static final Item toolHammerSteel = ItemHelper.createItem(MOD_ID, new SteelHammer("tool.pickaxe.hammer.steel", HammerConfig.cfg.getInt("Item IDs.SteelHammer"), HammerToolMaterial.steel), "tool.pickaxe.hammer.steel", "steel_hammer.png");
+	public static final Item toolHammerWooden = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/wooden_hammer")
+		.build(new WoodenHammer("tool.pickaxe.hammer.wooden", HammerConfig.cfg.getInt("Item IDs.WoodenHammer"), HammerToolMaterial.wood));
+
+	public static final Item toolHammerStone = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/stone_hammer")
+		.build(new StoneHammer("tool.pickaxe.hammer.stone", HammerConfig.cfg.getInt("Item IDs.StoneHammer"), HammerToolMaterial.stone));
+
+	public static final Item toolHammerIron = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/iron_hammer")
+		.build(new IronHammer("tool.pickaxe.hammer.iron", HammerConfig.cfg.getInt("Item IDs.IronHammer"), HammerToolMaterial.iron));
+
+	public static final Item toolHammerGold = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/golden_hammer")
+		.build(new GoldHammer("tool.pickaxe.hammer.gold", HammerConfig.cfg.getInt("Item IDs.GoldHammer"), HammerToolMaterial.gold));
+
+	public static final Item toolHammerDiamond = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/diamond_hammer")
+		.build(new DiamondHammer("tool.pickaxe.hammer.diamond", HammerConfig.cfg.getInt("Item IDs.DiamondHammer"), HammerToolMaterial.diamond));
+
+	public static final Item toolHammerSteel= new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/steel_hammer")
+		.build(new SteelHammer("tool.pickaxe.hammer.steel", HammerConfig.cfg.getInt("Item IDs.SteelHammer"), HammerToolMaterial.steel));
+
 
 	//excavators
-	public static final Item toolExcavatorWooden = ItemHelper.createItem(MOD_ID, new WoodenExcavator("tool.shovel.excavator.wooden", HammerConfig.cfg.getInt("Item IDs.WoodenExcavator"), HammerToolMaterial.wood), "tool.shovel.excavator.wooden", "wooden_excavator.png");
-	public static final Item toolExcavatorStone = ItemHelper.createItem(MOD_ID, new StoneExcavator("tool.shovel.excavator.stone", HammerConfig.cfg.getInt("Item IDs.StoneExcavator"), HammerToolMaterial.stone), "tool.shovel.excavator.stone", "stone_excavator.png");
-	public static final Item toolExcavatorIron = ItemHelper.createItem(MOD_ID, new IronExcavator("tool.shovel.excavator.iron",HammerConfig.cfg.getInt("Item IDs.IronExcavator"), HammerToolMaterial.iron), "tool.shovel.excavator.iron", "iron_excavator.png");
-	public static final Item toolExcavatorGolden = ItemHelper.createItem(MOD_ID, new GoldExcavator("tool.shovel.excavator.gold", HammerConfig.cfg.getInt("Item IDs.GoldExcavator"), HammerToolMaterial.gold), "tool.shovel.excavator.gold", "golden_excavator.png");
-	public static final Item toolExcavatorDiamond = ItemHelper.createItem(MOD_ID, new DiamondExcavator("tool.shovel.excavator.diamond", HammerConfig.cfg.getInt("Item IDs.DiamondExcavator"), HammerToolMaterial.diamond), "tool.shovel.excavator.diamond", "diamond_excavator.png");
-	public static final Item toolExcavatorSteel = ItemHelper.createItem(MOD_ID, new SteelExcavator("tool.shovel.excavator.steel", HammerConfig.cfg.getInt("Item IDs.SteelExcavator"), HammerToolMaterial.steel), "tool.shovel.excavator.steel", "steel_excavator.png");
+
+	public static final Item toolExcavatorWooden = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/wooden_excavator")
+		.build(new WoodenExcavator("tool.pickaxe.excavator.wooden", HammerConfig.cfg.getInt("Item IDs.WoodExcavator"), HammerToolMaterial.wood));
+
+	public static final Item toolExcavatorStone = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/stone_excavator")
+		.build(new StoneExcavator("tool.pickaxe.excavator.stone", HammerConfig.cfg.getInt("Item IDs.StoneExcavator"), HammerToolMaterial.stone));
+
+	public static final Item toolExcavatorIron = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/iron_excavator")
+		.build(new IronExcavator("tool.pickaxe.excavator.iron", HammerConfig.cfg.getInt("Item IDs.IronExcavator"), HammerToolMaterial.iron));
+	public static final Item toolExcavatorGolden = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/golden_excavator")
+		.build(new GoldExcavator("tool.pickaxe.excavator.gold", HammerConfig.cfg.getInt("Item IDs.GoldExcavator"), HammerToolMaterial.gold));
+
+	public static final Item toolExcavatorDiamond = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/diamond_excavator")
+		.build(new DiamondExcavator("tool.pickaxe.excavator.diamond", HammerConfig.cfg.getInt("Item IDs.DiamondExcavator"), HammerToolMaterial.diamond));
+
+	public static final Item toolExcavatorSteel = new ItemBuilder(MOD_ID)
+		.setIcon("hammers:item/steel_excavator")
+		.build(new WoodenExcavator("tool.pickaxe.excavator.steel", HammerConfig.cfg.getInt("Item IDs.SteelExcavator"), HammerToolMaterial.steel));
+
+
+
 
 	//vein axe
+
+	/*
 	public static final Item toolVeinAxeWooden = ItemHelper.createItem(MOD_ID, new WoodenVeinAxe("tool.axe.veinaxe.wooden", HammerConfig.cfg.getInt("Item IDs.WoodenVeinaxe"), HammerToolMaterial.wood), "tool.axe.veinaxe.wooden", "steel_excavator.png");
 	public static final Item toolVeinAxeStone = ItemHelper.createItem(MOD_ID, new StoneVeinAxe("tool.axe.veinaxe.stone", HammerConfig.cfg.getInt("Item IDs.StoneVeinaxe"), HammerToolMaterial.stone), "tool.axe.veinaxe.stone", "steel_excavator.png");
 	public static final Item toolVeinAxeIron = ItemHelper.createItem(MOD_ID, new IronVeinAxe("tool.axe.veinaxe.iron", HammerConfig.cfg.getInt("Item IDs.IronVeinaxe"), HammerToolMaterial.iron), "tool.axe.veinaxe.iron", "steel_excavator.png");
 	public static final Item toolVeinAxeGolden = ItemHelper.createItem(MOD_ID, new GoldVeinAxe("tool.axe.veinaxe.gold", HammerConfig.cfg.getInt("Item IDs.GoldVeinaxe"), HammerToolMaterial.gold), "tool.axe.veinaxe.gold", "steel_excavator.png");
 	public static final Item toolVeinAxeDiamond = ItemHelper.createItem(MOD_ID, new DiamondVeinAxe("tool.axe.veinaxe.diamond", HammerConfig.cfg.getInt("Item IDs.DiamondVeinaxe"), HammerToolMaterial.diamond), "tool.axe.veinaxe.diamond", "steel_excavator.png");
 	public static final Item toolVeinAxeSteel = ItemHelper.createItem(MOD_ID, new SteelVeinAxe("tool.axe.veinaxe.steel", HammerConfig.cfg.getInt("Item IDs.SteelVeinaxe"), HammerToolMaterial.steel), "tool.axe.veinaxe.steel", "steel_excavator.png");
-
+*/
 
 	@Override
 	public void onInitialize() {
@@ -205,5 +248,10 @@ public class Hammers implements ModInitializer, RecipeEntrypoint {
 			.addInput('T', Item.toolShovelSteel)
 			.addInput('R', Item.ingotSteel)
 			.create("toolExcavatorSteel", new ItemStack(toolExcavatorSteel, 1));
+	}
+
+	@Override
+	public void initNamespaces() {
+
 	}
 }
